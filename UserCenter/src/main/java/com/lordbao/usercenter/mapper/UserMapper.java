a5977418654c6192d83667a52f0f8cad1857f873
LordBao666 <916900021@qq.com>
2024-09-20 17:07:26 +0800

    Initialize user center project

---
+++ UserCenter/src/main/java/com/lordbao/usercenter/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.lordbao.usercenter.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lordbao.usercenter.pojo.User;
+
+/**
+ * @Author Lord_Bao
+ * @Date 2024/9/20 16:17
+ * @Version 1.0
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
